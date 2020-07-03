3ccb54e7ff2925ed49b61915e8053b59435aaa2f
Luccas de Barros <ldbcandido@gmail.com>
2020-07-03 17:59:25 -0300

    index responsive layout modified (pure css)

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ if (window.SimpleForm) {
   new SimpleForm({
     form: ".formphp", // seletor do formulário
     button: "#enviar", // seletor do botão
-    erro: "<div id='form-erro'><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>", // mensagem de erro
+    erro: "<div id='form-erro'><p>Um erro ocorreu, tente para o email contato@ahbatata.com.br</p></div>", // mensagem de erro
     sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
   });
 }
